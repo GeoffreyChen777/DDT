fix eig sign

pca_project.py
@@ -19,13 +19,14 @@ class PCAProjectNet(nn.Module):
 
         first_compo = eigvec[:, 0]
 
+        projected_map = torch.matmul(first_compo.unsqueeze(0), reshaped_features).view(1, features.size(0), -1)\
+            .view(features.size(0), features.size(2), features.size(3))
+
         maxv = projected_map.max()
         minv = projected_map.min()
 
         projected_map *= (maxv + minv) / torch.abs(maxv + minv)
-
-        projected_map = torch.matmul(first_compo.unsqueeze(0), reshaped_features).view(1, features.size(0), -1)\
-            .view(features.size(0), features.size(2), features.size(3))
+        
         return projected_map
 
 